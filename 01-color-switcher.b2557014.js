d0e20a66ddc9131f5b4d5ee7c5b63f5b31ed3b90
wanyyf <102466744+wanyyf@users.noreply.github.com>
2022-07-08 08:58:27 +0000

    Deploying to gh-pages from @ wanyyf/goit-js-hw-09@0c4e26b90a69f845fa7036643682496277171cfc 🚀

--- 01-color-switcher.b2557014.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let o=null;t.addEventListener("click",(()=>{o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(o)}));
-//# sourceMappingURL=01-color-switcher.b2557014.js.map
