8eb6cfb04376cc57845772bc877c97f5e98811c1
wanyyf <vanyaposta@gmail.com>
2022-07-08 10:40:56 +0300

    all

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,21 @@
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+let changeColor = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const onStartBtnClick = () => {
+  changeColor = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtnEl.disabled = true;
+};
+
+const onStopBtnClick = () => {
+  clearInterval(changeColor);
+};
+
+startBtnEl.addEventListener('click', onStartBtnClick);
+stopBtnEl.addEventListener('click', onStopBtnClick);
